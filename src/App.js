12d832592fbc9c25e5720e1da195d716e22377f2
Iancu Andrei <29271791+iancuandrei@users.noreply.github.com>
2020-07-12 19:27:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Quiz from "./components/Quiz";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [questions, setQuestions] = useState();
+	const [questionsNum, setQuestionsNum] = useState(10);
+	const [category, setCategory] = useState("any");
+	const [difficulty, setDifficulty] = useState("any");
+	const [answerType, setAnswerType] = useState("any");
+
+	let query = `https://opentdb.com/api.php?amount=${questionsNum}${category !== "any" && "&category=" + category}${
+		difficulty !== "any" && "&difficulty=" + difficulty
+	}${answerType !== "any" && "&type=" + answerType}`;
+
+	query = query.replace(/false/gm, "");
+
+	useEffect(() => {
+		fetch("https://opentdb.com/api.php?amount=10")
+			.then((res) => res.json())
+			.then((data) => setQuestions(data.results));
+	}, []);
+
+	const startGame = () => {
+		fetch(query)
+			.then((res) => res.json())
+			.then((data) => setQuestions(data.results));
+	};
+
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<div className="App">
+						<h1>Wild Trivia</h1>
+						<div className="optionsContainer">
+							<div className="column">
+								<label for="trivia_amount">Number of Questions (50 Max):</label>
+								<input
+									className="input"
+									onChange={(e) => setQuestionsNum(e.target.value)}
+									type="number"
+									name="trivia_amount"
+									min="1"
+									value={questionsNum}
+									max="50"
+								/>
+							</div>
+
+							<div className="column">
+								<label for="trivia_category">Select Category: </label>
+								<select onChange={(e) => setCategory(e.target.value)} className="input" name="trivia_category">
+									<option value="any">Any Category</option>
+									<option value="9">General Knowledge</option>
+									<option value="10">Entertainment: Books</option>
+									<option value="11">Entertainment: Film</option>
+									<option value="12">Entertainment: Music</option>
+									<option value="13">Entertainment: Musicals &amp; Theatres</option>
+									<option value="14">Entertainment: Television</option>
+									<option value="15">Entertainment: Video Games</option>
+									<option value="16">Entertainment: Board Games</option>
+									<option value="17">Science &amp; Nature</option>
+									<option value="18">Science: Computers</option>
+									<option value="19">Science: Mathematics</option>
+									<option value="20">Mythology</option>
+									<option value="21">Sports</option>
+									<option value="22">Geography</option>
+									<option value="23">History</option>
+									<option value="24">Politics</option>
+									<option value="25">Art</option>
+									<option value="26">Celebrities</option>
+									<option value="27">Animals</option>
+									<option value="28">Vehicles</option>
+									<option value="29">Entertainment: Comics</option>
+									<option value="30">Science: Gadgets</option>
+									<option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+									<option value="32">Entertainment: Cartoon &amp; Animations</option>{" "}
+								</select>
+							</div>
+							<div className="column">
+								<label for="trivia_difficulty">Select Difficulty: </label>
+
+								<select onChange={(e) => setDifficulty(e.target.value)} className="input" name="trivia_difficulty">
+									<option value="any">Any Difficulty</option>
+									<option value="easy">Easy</option>
+									<option value="medium">Medium</option>
+									<option value="hard">Hard</option>
+								</select>
+							</div>
+							<div className="column">
+								<label for="trivia_type">Select Type: </label>
+
+								<select onChange={(e) => setAnswerType(e.target.value)} className="input" name="trivia_type">
+									<option value="any">Any Type</option>
+									<option value="multiple">Multiple Choice</option>
+									<option value="boolean">True / False</option>
+								</select>
+							</div>
+						</div>
+						<Link to="quiz">
+							<button onClick={startGame} className="startBtn">
+								Start
+							</button>
+						</Link>
+					</div>
+				</Route>
+				<Route path="/quiz">
+					<Quiz questions={questions} />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/Quiz.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from "react-html-parser";
+
+import "./Quiz.css";
+
+export default function Quiz(props) {
+	const [counter, setCounter] = useState(0);
+	const [score, setScore] = useState(0);
+	const [correct, setCorrect] = useState();
+	const [clicked, setClicked] = useState(false);
+	const [finished, setFinished] = useState(false);
+	const answers = [props.questions[counter].correct_answer, ...props.questions[counter].incorrect_answers].sort();
+
+	const submitAnswers = (e) => {
+		if (!clicked) {
+			if (e.target.value === props.questions[counter].correct_answer) {
+				setScore(score + 1);
+				setCorrect(true);
+			} else {
+				setCorrect(false);
+			}
+		}
+
+		setClicked(true);
+	};
+
+	const nextQuestion = () => {
+		if (counter < props.questions.length - 1) {
+			setCounter(counter + 1);
+		} else {
+			setFinished(true);
+		}
+		setClicked(false);
+	};
+
+	return (
+		<div className="quizContainer">
+			{!finished ? (
+				<div className="column2">
+					<div className="questionScore">
+						<h2 className="questionNum">
+							Question:
+							<span>
+								{counter + 1} / {props.questions.length}
+							</span>
+						</h2>
+						<h2 className="score">
+							Score: <span>{score}</span>
+						</h2>
+					</div>
+					<h2 className="question">{ReactHtmlParser(props.questions[counter].question)}</h2>
+					<div className="answers">
+						{answers.map((item) => {
+							return (
+								<button className="answer" onClick={(e) => submitAnswers(e)} value={item}>
+									{ReactHtmlParser(item)}
+								</button>
+							);
+						})}
+					</div>
+					{clicked && (
+						<div className="rightWrongContainer">
+							<h2 className="rightWrong">
+								{correct ? (
+									<span className="correctAnswer">Correct!</span>
+								) : (
+									<span className="wrongAnswer">
+										Incorrect, the right answer is <span>{ReactHtmlParser(props.questions[counter].correct_answer)}</span>
+									</span>
+								)}
+							</h2>
+							<button className="next" onClick={nextQuestion}>
+								Next
+							</button>
+						</div>
+					)}
+				</div>
+			) : (
+				<div className="finishedContainer">
+					<h2 className="finished">
+						<span>
+							{score} / {props.questions.length}
+						</span>{" "}
+						answered correctly.
+					</h2>
+					<Link to="/">
+						<button>Go Home</button>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
+}
